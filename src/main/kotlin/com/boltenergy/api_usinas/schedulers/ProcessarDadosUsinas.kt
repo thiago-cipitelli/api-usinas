e0f9b737685f68151f654d73198132fc52d87111
Thiago Cipitelli <cipitelliprata@gmail.com>
2026-01-29 15:55:59 -0300

    feat: adiciona logger e tratamento de erro

--- src/main/kotlin/com/boltenergy/api_usinas/schedulers/ProcessarDadosUsinas.kt
+++ src/main/kotlin/com/boltenergy/api_usinas/schedulers/ProcessarDadosUsinas.kt
@@ -3,13 +3,22 @@ package com.boltenergy.api_usinas.schedulers
 import com.boltenergy.api_usinas.services.RalieUsinasService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.slf4j.LoggerFactory
 
 
 @Component
 class ProcessarDadosUsinas (private val ralieUsinas: RalieUsinasService){
+
+    private val logger = LoggerFactory.getLogger(ProcessarDadosUsinas::class.java)
+
     @Scheduled(cron = "0 0 9 * * *")
     fun executar(){
-        ralieUsinas.downloadFile()
-        ralieUsinas.processarCsv(usinaRepository = )
+        try {
+            logger.info("Iniciando processamento das usinas")
+            ralieUsinas.downloadFile()
+            logger.info("Processamento finalizado")
+        } catch (ex: Exception) {
+            logger.error("Erro ao processar usinas", ex)
+        }
     }
 }
