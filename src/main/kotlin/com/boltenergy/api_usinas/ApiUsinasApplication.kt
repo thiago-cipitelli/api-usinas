e6e683e91de6d15ab55a174304b15808d0b3e27b
Thiago Cipitelli <cipitelliprata@gmail.com>
2026-01-29 11:15:03 -0300

    setup projeto

---
+++ src/main/kotlin/com/boltenergy/api_usinas/ApiUsinasApplication.kt
@@ -0,0 +1,25 @@
+package com.boltenergy.api_usinas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@SpringBootApplication(
+    exclude = [
+        DataSourceAutoConfiguration::class,
+        HibernateJpaAutoConfiguration::class
+    ]
+)
+class ApiUsinasApplication {
+
+    @GetMapping
+    fun helloWorld() = "Hello"
+}
+
+fun main(args: Array<String>) {
+	runApplication<ApiUsinasApplication>(*args)
+}
---
+++ src/test/kotlin/com/boltenergy/api_usinas/ApiUsinasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boltenergy.api_usinas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiUsinasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
