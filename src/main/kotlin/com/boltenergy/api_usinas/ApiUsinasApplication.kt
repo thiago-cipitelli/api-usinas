7667779439f11a5d6ea6ec1266bc735cb12fc88a
Thiago Cipitelli <cipitelliprata@gmail.com>
2026-01-30 11:07:55 -0300

    feat: adiciona data_ralie no retorno json do ranking

--- src/main/kotlin/com/boltenergy/api_usinas/ApiUsinasApplication.kt
+++ src/main/kotlin/com/boltenergy/api_usinas/ApiUsinasApplication.kt
@@ -3,6 +3,7 @@ package com.boltenergy.api_usinas
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration
+import org.springframework.boot.persistence.autoconfigure.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,11 +11,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @EnableScheduling
+@EntityScan("com.boltenergy.api_usinas.models")
 @SpringBootApplication
 class ApiUsinasApplication {
 
-    @GetMapping
-    fun helloWorld() = "Hello"
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/boltenergy/api_usinas/repositories/UsinaRepository.kt
+++ src/main/kotlin/com/boltenergy/api_usinas/repositories/UsinaRepository.kt
@@ -3,13 +3,16 @@ package com.boltenergy.api_usinas.repositories
 import com.boltenergy.api_usinas.models.Usina
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.query.DeclaredQuery.nativeQuery
 import org.springframework.stereotype.Repository
 import java.math.BigDecimal
+import java.time.LocalDate
 
 interface RankingPotencia {
     val nome: String
     val ceg: String
     val potencia: BigDecimal
+    val dataRalie: LocalDate
 }
 
 @Repository
@@ -17,13 +20,21 @@ interface UsinaRepository : JpaRepository<Usina, String> {
     @Query("SELECT u.ceg FROM Usina u WHERE u.ceg IN :cegs")
     fun findAllCegByCegIn(cegs: Set<String>): List<String>
 
-    @Query("""
-        SELECT u.nome as nome, u.ceg as ceg, MAX(r.potenciaOutorgada) as potencia
-        FROM Usina u
-        JOIN ralie_publicacoes r ON u.ceg = r.usina.ceg
-        GROUP BY u.nome, u.ceg
+    @Query(value = """
+       SELECT nome, ceg, potencia, data_ralie
+        FROM (
+            SELECT 
+                u.nome as nome, 
+                u.ceg as ceg, 
+                r.potencia_outorgada as potencia,
+                r.data_publicacao as data_ralie,
+                ROW_NUMBER() OVER (PARTITION BY u.ceg ORDER BY r.potencia_outorgada DESC, r.data_publicacao DESC) as rn
+            FROM usinas u
+            JOIN ralie_publicacoes r ON u.ceg = r.ceg
+        ) as sub
+        WHERE rn = 1
         ORDER BY potencia DESC
         LIMIT 5
-    """)
+    """, nativeQuery = true)
     fun findTop5UsinasMaisPotentes(): List<RankingPotencia>
 }
